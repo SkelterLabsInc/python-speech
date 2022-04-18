486e55790dbd337d6ae92850e2b548e8b1b36973
Jihun Park <jihunlorenzopark@wisefour.com>
2022-04-18 13:25:06 +0900

    (MNT-608) Fix steam2 UsageError when stream request sent
    Change-Id: I1c58ed5f3d69b70da2ce08886c4a6fb1ada3bbd0

--- utils/grpc_utils.py
+++ utils/grpc_utils.py
@@ -3,6 +3,7 @@
 import os
 
 import grpc
+from grpc import aio
 
 
 class AuthGateway(grpc.AuthMetadataPlugin):
@@ -65,11 +66,53 @@ class GenericClientInterceptor(grpc.UnaryUnaryClientInterceptor,
         return postprocess(response_it) if postprocess else response_it
 
 
-def additional_headers_interceptor(headers):
+class AsyncGenericClientInterceptor(aio.StreamStreamClientInterceptor,
+                                    aio.StreamUnaryClientInterceptor,
+                                    aio.UnaryStreamClientInterceptor,
+                                    aio.UnaryUnaryClientInterceptor):
+    """
+    This is adaptor for aio client interceptors.
+    """
+
+    def __init__(self, interceptor_function):
+        # pylint: disable=super-init-not-called
+        self._fn = interceptor_function
+
+    async def intercept_unary_unary(self, continuation, client_call_details,
+                                    request):
+        new_details, new_request_iterator, postprocess = self._fn(
+            client_call_details, iter((request,)), False, False)
+        response = continuation(new_details, next(new_request_iterator))
+        return postprocess(response) if postprocess else response
+
+    async def intercept_unary_stream(self, continuation, client_call_details,
+                                     request):
+        new_details, new_request_iterator, postprocess = self._fn(
+            client_call_details, iter((request,)), False, True)
+        response_it = continuation(new_details, next(new_request_iterator))
+        return postprocess(response_it) if postprocess else response_it
+
+    async def intercept_stream_unary(self, continuation, client_call_details,
+                                     request_iterator):
+        new_details, new_request_iterator, postprocess = self._fn(
+            client_call_details, request_iterator, True, False)
+        response = continuation(new_details, new_request_iterator)
+        return postprocess(response) if postprocess else response
+
+    async def intercept_stream_stream(self, continuation, client_call_details,
+                                      request_iterator):
+        new_details, new_request_iterator, postprocess = self._fn(
+            client_call_details, request_iterator, True, True)
+        response_it = continuation(new_details, new_request_iterator)
+        return postprocess(response_it) if postprocess else response_it
+
+
+def additional_headers_interceptor(headers, is_aio=False):
     """Return interceptor which adds given headers to each calls.
 
     Args:
         headers: The list of header name, header value pair.
+        is_aio: a flag if interceptor is for aio
     """
 
     def _intercept_call(client_call_details, request_iterator,
@@ -83,6 +126,8 @@ def additional_headers_interceptor(headers):
         client_call_details = client_call_details._replace(metadata=metadata)
         return client_call_details, request_iterator, None
 
+    if is_aio:
+        return AsyncGenericClientInterceptor(_intercept_call)
     return GenericClientInterceptor(_intercept_call)
 
 
@@ -110,7 +155,10 @@ def create_credentials(api_key):
                                               call_credentials)
 
 
-def create_channel(api_url, api_key=None, insecure=None):
+def create_channel(api_url,
+                   api_key=None,
+                   insecure=None,
+                   additional_headers=None):
     """Create gRPC channel.
 
     Args:
@@ -119,20 +167,68 @@ def create_channel(api_url, api_key=None, insecure=None):
         insecure: Skip server certificate and domain verification.
             If it is None and api_key is specified, insecure is False.
             If it is None and api_key is unspecified, insecure is True.
+        additional_headers: header for intercepting key
 
     Returns:
         grpc.Channel
     """
+    if additional_headers is None:
+        additional_headers = []
+
     if insecure is None:
         insecure = not bool(api_key)
 
-    if not insecure:
+    if insecure:
+        channel = grpc.insecure_channel(api_url)
+        if api_key:
+            additional_headers.append(('x-api-key', api_key))
+    else:
         credentials = create_credentials(api_key)
-        return grpc.secure_channel(api_url, credentials)
+        channel = grpc.secure_channel(api_url, credentials)
 
-    channel = grpc.insecure_channel(api_url)
-    if not api_key:
+    if not additional_headers:
         return channel
 
-    interceptor = additional_headers_interceptor([('x-api-key', api_key)])
+    interceptor = additional_headers_interceptor(additional_headers)
     return grpc.intercept_channel(channel, interceptor)
+
+
+def create_aio_channel(api_url,
+                       api_key=None,
+                       insecure=None,
+                       additional_headers=None):
+    """Create aio gRPC channel.
+
+    Args:
+        api_url: AIQ API url.
+        api_key: AIQ API key.
+        insecure: Skip server certificate and domain verification.
+            If it is None and api_key is specified, insecure is False.
+            If it is None and api_key is unspecified, insecure is True.
+        additional_headers: custom headers
+
+    Returns:
+        grpc.aio.Channel
+    """
+    if additional_headers is None:
+        additional_headers = []
+
+    if insecure is None:
+        insecure = not bool(api_key)
+
+    if insecure:
+        interceptor = None
+        if api_key:
+            additional_headers.append(('x-api-key', api_key))
+            interceptor = additional_headers_interceptor(
+                additional_headers, True)
+
+        if interceptor:
+            channel = aio.insecure_channel(api_url, interceptors=[interceptor])
+        else:
+            channel = aio.insecure_channel(api_url)
+    else:
+        credentials = create_credentials(api_key)
+        channel = aio.secure_channel(api_url, credentials)
+
+    return channel
